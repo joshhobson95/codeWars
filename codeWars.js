709d097023f230f8f827170378a07eb89dfea4a5
Josh Hobson <joshuahobson1995@gmail.com>
2023-01-05 13:30:15 -0700

    warming up

--- codeWars.js
+++ codeWars.js
@@ -167,6 +167,49 @@ for ( let i =0; i < numOfZeroes; i++){
 return filtered
 }
 
+//& 1/5/23
+
+// Move the first letter of each word to the end of it, then add "ay" to the end of the word. Leave punctuation marks untouched.
+
+function pigIt(str){
+  let words = str.split(" ");
+  let newArr = []
+  
+  newStr = "";
+  
+  for (let i = 0; i < words.length; i++){
+      newStr = words[i].slice(1) + words[i].charAt(0) + "ay"; 
+      newArr.push(newStr);
+    } return newArr.join(" ")
+}
+
+// The marketing team is spending way too much time typing in hashtags.
+// Let's help them with our own Hashtag Generator!
+
+// Here's the deal:
+
+//     It must start with a hashtag (#).
+//     All words must have their first letter capitalized.
+//     If the final result is longer than 140 chars it must return false.
+//     If the input or the result is an empty string it must return false.
 
 
-
+function generateHashtag (str) {
+  let wordArray = str.split(' ').filter(char => char !== "");
+  let result = "#";
+  
+  if (wordArray.length === 0) {
+    return false;
+  };
+  
+  result = result + wordArray.map(word => {
+    let capitalizedWord = word.charAt(0).toUpperCase() + word.slice(1);
+    return capitalizedWord;
+  }).join('');
+  if(result.length > 140) {
+    return false;
+  } else{
+    return result;
+  };
+  
+};
