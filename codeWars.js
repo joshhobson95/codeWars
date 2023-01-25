9c185ee1fa7efeb168fb582052207a99b73e707d
Josh Hobson <joshuahobson1995@gmail.com>
2023-01-25 10:42:48 -0700

    warm

--- codeWars.js
+++ codeWars.js
@@ -271,7 +271,7 @@ console.log(realNums)
 
 
 let letters = chars.filter(char => /[a-zA-Z]/.test(char));
-console.log(lette
+console.log(letters)
   }
   
   else {
@@ -286,3 +286,35 @@ console.log(lette
 function incrementString(str) {
   return str.replace(/([0-8]|\d?9+)?$/, (e) => (e ? +e + 1 : 1));
 }
+
+
+
+
+//& 1/25/2023
+
+// Some numbers have funny properties. For example:
+
+//     89 --> 8¹ + 9² = 89 * 1
+
+//     695 --> 6² + 9³ + 5⁴= 1390 = 695 * 2
+
+//     46288 --> 4³ + 6⁴+ 2⁵ + 8⁶ + 8⁷ = 2360688 = 46288 * 51
+
+// Given a positive integer n written as abcd... (a, b, c, d... being digits) and a positive integer p
+
+//     we want to find a positive integer k, if it exists, such that the sum of the digits of n taken to the successive powers of p is equal to k * n.
+
+// In other words:
+
+//     Is there an integer k such as : (a ^ p + b ^ (p+1) + c ^(p+2) + d ^ (p+3) + ...) = n * k
+
+// If it is the case we will return k, if not return -1.
+
+// Note: n and p will always be given as strictly positive integers.
+
+
+
+function digPow(n, p){
+  let arr=n.toString().split('').map(Number).reduce((acc,cur,i,arr)=>acc+(Math.pow(arr[i],p+i)),0)/n
+  return (''+arr).includes('.')?-1:arr
+  }
